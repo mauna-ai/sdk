9dcb7ce9bf48fd96a071a32e75bf0a9b1cb07612
Diwank Singh Tomer <diwank.singh@gmail.com>
2021-03-01 14:06:25 +0000

    fix: Use direct endpoint for now
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,8 @@ export const ivLength = 16;
 export const defaultIv = crypto.randomBytes(ivLength);
 export const encryptionKeyLenBytes = 64;
 export const apiEndpoint = "https://api.mauna.cloud/v1/graphql";
-export const authEndpoint = "https://sdk.auth.mauna.cloud";
+export const authEndpoint = "https://us-central1-mauna-ai-290409.cloudfunctions.net/generateHasuraJWT";
+// export const authEndpoint = "https://sdk.auth.mauna.cloud";
 
 export default {
   apiEndpoint,
