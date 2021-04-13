6d834e71936f18720527883ea233463012494858
Dmitry Paramonov <asmatic075@gmail.com>
2021-04-13 13:47:44 +0300

    feat: Add an ability to set API and auth endpoints via environment variables

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ export const cipherMode = "aes-256-cbc";
 export const ivLength = 16;
 export const defaultIv = Buffer.alloc(16, 0);
 export const encryptionKeyLenBytes = 64;
-export const apiEndpoint = "https://api.mauna.cloud/v1/graphql";
-export const authEndpoint = "https://sdk.mauna.cloud/generateHasuraJWT";
+export const apiEndpoint = process.env.MAUNA_API_ENDPOINT || "https://api.mauna.cloud/v1/graphql";
+export const authEndpoint = process.env.MAUNA_AUTH_ENDPOINT || "https://sdk.mauna.cloud/generateHasuraJWT";
 
 export default {
   apiEndpoint,
